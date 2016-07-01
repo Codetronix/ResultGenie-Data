542bf1b16d107eb1063aefa5b725f4cbe6b66393
kingspp <kingspprathyush@gmail.com>
2016-07-02 01:42:01 +0530

    Added real world Subjects

---
+++ python/VTU.py
@@ -0,0 +1,460 @@
+class EC(object):
+    S1 = [
+        ('Engineering Mathematics 1', 'theory'),
+        ('Engineering Physics', 'theory'),
+        ('Engineering Chemistry','theory'),
+        ('Elements of Civil engineering', 'theory'),
+        ('Programming in C and Data structures', 'theory'),
+        ('Elements of Mechanical Engineering', 'theory'),
+        ('Chemistry Lap', 'lab'),
+        ('Computer Lab', 'lab')
+
+    ]
+
+    S2 = [
+        ('Basic Electronics', 'theory'),
+        ('Computer Aided Drawing', 'theory'),
+        ('Basic Electrical Engineering', 'theory'),
+        ('Engineering Mathematics 2', 'theory'),
+        ('English', 'theory'),
+        ('Constitution', 'theory'),
+        ('Physics Lab', 'lab'),
+        ('Workshop', 'lab')
+    ]
+
+    S3 = [
+        ('Engineering Mathematics 3', 'theory'),
+        ('Analog Electrical Circuits', 'theory'),
+        ('Logic Design', 'theory'),
+        ('Network Analysis', 'theory'),
+        ('Instrumentation', 'theory'),
+        ('Field Theory', 'theory'),
+        ('AEC Lab', 'lab'),
+        ('LD Lab','lab')
+    ]
+
+    S4 = [
+        ('Engineering Mathematics 4', 'theory'),
+        ('Microcontrollers', 'theory'),
+        ('Control Systems', 'theory'),
+        ('Signals and Systems', 'theory'),
+        ('Fundamentals of HDL', 'theory'),
+        ('Linear IC', 'theory'),
+        ('Microcontrollers Lab', 'lab'),
+        ('HDL  Lab', 'lab'),
+    ]
+
+    S5 = [
+        ('Management and Entrepreneurship', 'theory'),
+        ('Digital Signal Processing', 'theory'),
+        ('Analog Communication', 'theory'),
+        ('Microwaves and Radar', 'theory'),
+        ('Information Theory and Coding', 'theory'),
+        ('Fundamentals of CMOS and VLSI', 'theory'),
+        ('Digital Signal Processing Lab', 'lab'),
+        ('Analog Communications Lab', 'lab')
+    ]
+
+    S6 = [
+        ('Digital Communication', 'theory'),
+        ('Microprocessor', 'theory'),
+        ('Microelectronics Circuits', 'theory'),
+        ('Antennas and Propagation', 'theory'),
+        ('Operating Systems', 'theory'),
+        ('Analog and mixed mode VLSI', 'theory'),
+        ('Advanced Communication Lab', 'lab'),
+        ('Microprocessor Lab', 'lab')
+    ]
+
+    S7 = [
+        ('Computer Communication Networks', 'theory'),
+        ('Optical Fiber Communication', 'theory'),
+        ('Power Electronics', 'theory'),
+        ('Embedded VLSI Design', 'theory'),
+        ('DSP Algorithms and Architecture', 'theory'),
+        ('Real Time Systems', 'theory'),
+        ('VLSI Lab', 'lab'),
+        ('Power Electronics Lab', 'lab')
+    ]
+
+    S8 =[
+        ('Wireless Communication', 'theory'),
+        ('Digital Switching Systems', 'theory'),
+        ('Distributed Systems', 'theory'),
+        ('Network Security', 'theory'),
+        ('Optical Networks', 'theory'),
+        ('Internet Engineering', 'theory'),
+        ('Project', 'lab'),
+        ('Seminar', 'lab')
+    ]
+
+    EC = [S1, S2, S3, S4, S5, S6, S7, S8]
+
+
+class MECHANICAL(object):
+    S1 = [
+        ('Engineering Mathematics 1', 'theory'),
+        ('Engineering Physics', 'theory'),
+        ('Engineering Chemistry', 'theory'),
+        ('Elements of Civil engineering', 'theory'),
+        ('Programming in C and Data structures', 'theory'),
+        ('Elements of Mechanical Engineering', 'theory'),
+        ('Chemistry Lap', 'lab'),
+        ('Computer Lab', 'lab')
+
+    ]
+
+    S2 = [
+        ('Basic Electronics', 'theory'),
+        ('Computer Aided Drawing', 'theory'),
+        ('Basic Electrical Engineering', 'theory'),
+        ('Engineering Mathematics 2', 'theory'),
+        ('English', 'theory'),
+        ('Constitution', 'theory'),
+        ('Physics Lab', 'lab'),
+        ('Workshop', 'lab')
+    ]
+
+    S3 = [
+        ('Engineering Mathematics 3', 'theory'),
+        ('Material Science and Metallurgy', 'theory'),
+        ('Mechanical Measurements and Metrology', 'theory'),
+        ('Basic Thermodynamics', 'theory'),
+        ('Mechanics of Materials', 'theory'),
+        ('Manufacturing Process 1', 'theory'),
+        ('Computer Aided Machine Drawing', 'lab'),
+        ('Foundry Lab', 'lab')
+    ]
+
+    S4 = [
+        ('Engineering Mathematics 4', 'theory'),
+        ('Applied Thermodynamics', 'theory'),
+        ('Kinematics of Machines', 'theory'),
+        ('Manufacturing Process 3', 'theory'),
+        ('Fluid Mechanics', 'theory'),
+        ('Machine shop', 'theory'),
+        ('Material Testing lab', 'lab'),
+        ('Metrology Lab', 'lab')
+    ]
+
+    S5 = [
+        ('Management and Entrepreneurship', 'theory'),
+        ('Design of Machine Elements 1',  'theory'),
+        ('Energy Engineering', 'theory'),
+        ('Dynamics of Machines', 'theory'),
+        ('Manufacturing Process 3', 'theory'),
+        ('Turbo Machines', 'theory'),
+        ('Fluid Mechanics Lab', 'lab'),
+        ('Energy lab', 'lab')
+    ]
+
+    S6 =[
+        ('Computer Integrated Manufacturing', 'theory'),
+        ('Design of Machine Elements 2', 'theory'),
+        ('Heat and Mass Transfer', 'theory'),
+        ('Finite Element Methods', 'theory'),
+        ('Mechatronics and Microprocessor', 'theory'),
+        ('Theory of Elasticity', 'theory'),
+        ('Heat and Mass Transfer Lab', 'lab'),
+        ('Computer Aided Lab', 'lab')
+    ]
+
+    S7 = [
+        ('Engineering Economy', 'theory'),
+        ('Mechanical Vibrations', 'theory'),
+        ('Hydraulics and Pneumatics', 'theory'),
+        ('Operation Research', 'theory'),
+        ('Mechanism Design', 'theory'),
+        ('Theory of Plasticity', 'theory'),
+        ('Design Lab', 'lab'),
+        ('Automation Lab', 'lab')
+    ]
+
+    S8 = [
+        ('Operation Management', 'theory'),
+        ('Control Engineering', 'theory'),
+        ('Tribology', 'theory'),
+        ('Fracture Mechanics', 'theory'),
+        ('Power Plant Engineering', 'theory'),
+        ('Nanotechnology', 'theory'),
+        ('Project Work', 'lab'),
+        ('Seminar', 'lab')
+    ]
+
+    MECHANICAL = [S1, S2, S3, S4, S5, S6, S7, S8]
+
+
+class IS(object):
+    S1 = [
+        ('Engineering Mathematics 1', 'theory'),
+        ('Engineering Physics', 'theory'),
+        ('Engineering Chemistry', 'theory'),
+        ('Elements of Civil engineering', 'theory'),
+        ('Programming in C and Data structures', 'theory'),
+        ('Elements of Mechanical Engineering', 'theory'),
+        ('Chemistry Lap', 'lab'),
+        ('Computer Lab', 'lab')
+    ]
+
+    S2 = [
+        ('Basic Electronics', 'theory'),
+        ('Computer Aided Drawing', 'theory'),
+        ('Basic Electrical Engineering', 'theory'),
+        ('Engineering Mathematics 2', 'theory'),
+        ('English', 'theory'),
+        ('Constitution', 'theory'),
+        ('Physics Lab', 'lab'),
+        ('Workshop', 'lab')
+    ]
+
+    S3 = [
+        ('Engineering Mathematics 3', 'theory'),
+        ('Electronics Circuits', 'theory'),
+        ('Logic Design', 'theory'),
+        ('Discrete Mathematical Structures', 'theory'),
+        ('Data structures with C', 'theory'),
+        ('Oops with C++', 'theory'),
+        ('DS with C++', 'lab'),
+        ('EC and LD Lab', 'lab'),
+    ]
+
+    S4= [
+        ('Engineering Mathematics 4', 'theory'),
+        ('Graph Theory', 'theory'),
+        ('Design and Analysis of Algorithm', 'theory'),
+        ('Unix and Shell Programming', 'theory'),
+        ('Microprocessors', 'theory'),
+        ('Computer Organization', 'theory'),
+        ('Design and Analysis of Algorithms Lab', 'lab'),
+        ('Microprocessors Lab', 'lab')
+    ]
+
+    S5 = [
+        ('Software Engineering', 'theory'),
+        ('System Software', 'theory'),
+        ('Operating System', 'theory'),
+        ('Database Management Systems', 'theory'),
+        ('Computer Networks 1', 'theory'),
+        ('Formal Languages and Automata Theory', 'theory'),
+        ('Database Lab', 'lab'),
+        ('Operating Systems Lab', 'lab')
+    ]
+
+    S6 = [
+        ('Management and Entrepreneurship', 'theory'),
+        ('UNIX System Programming', 'theory'),
+        ('File Structures', 'theory'),
+        ('Computer Networks 2', 'theory'),
+        ('Software Testing', 'theory'),
+        ('Operations Research', 'theory'),
+        ('File Structures Lab', 'lab'),
+        ('Software Testing Lab', 'lab')
+    ]
+
+    S7 = [
+        ('Objected Oriented Design', 'theory'),
+        ('Information Systems', 'theory'),
+        ('Programming the Web', 'theory'),
+        ('Data Warehousing and Data Mining', 'theory'),
+        ('Advanced DBMS', 'theory'),
+        ('JAVA and J2EE', 'theory'),
+        ('JAVA Lab', 'lab'),
+        ('Web Lab', 'lab')
+    ]
+
+    S8 = [
+        ('Software Architectures', 'theory'),
+        ('System Modelling and Simulation', 'theory'),
+        ('Wireless Networks', 'theory'),
+        ('User Interface Design', 'theory'),
+        ('Information Theory and Coding', 'theory'),
+        ('Network Management Systems', 'theory'),
+        ('Project Work', 'lab'),
+        ('Seminar', 'lab')
+    ]
+
+    IS = [S1, S2, S3, S4, S5, S6, S7, S8]
+
+
+class CS(object):
+    S1 = [
+        ('Engineering Mathematics 1', 'theory'),
+        ('Engineering Physics', 'theory'),
+        ('Engineering Chemistry', 'theory'),
+        ('Elements of Civil engineering', 'theory'),
+        ('Programming in C and Data structures', 'theory'),
+        ('Elements of Mechanical Engineering', 'theory'),
+        ('Chemistry Lap', 'lab'),
+        ('Computer Lab', 'lab')
+    ]
+
+    S2 = [
+        ('Basic Electronics', 'theory'),
+        ('Computer Aided Drawing', 'theory'),
+        ('Basic Electrical Engineering', 'theory'),
+        ('Engineering Mathematics 2', 'theory'),
+        ('English', 'theory'),
+        ('Constitution', 'theory'),
+        ('Physics Lab', 'lab'),
+        ('Workshop', 'lab')
+    ]
+
+    S3 = [
+        ('Engineering Mathematics 3', 'theory'),
+        ('Electronics Circuits', 'theory'),
+        ('Logic Design', 'theory'),
+        ('Discrete Mathematical Structures', 'theory'),
+        ('Data structures with C', 'theory'),
+        ('Oops with C++', 'theory'),
+        ('DS with C++', 'lab'),
+        ('EC and LD Lab', 'lab'),
+    ]
+
+    S4= [
+        ('Engineering Mathematics 4', 'theory'),
+        ('Graph Theory', 'theory'),
+        ('Design and Analysis of Algorithm', 'theory'),
+        ('Unix and Shell Programming', 'theory'),
+        ('Microprocessors', 'theory'),
+        ('Computer Organization', 'theory'),
+        ('Design and Analysis of Algorithms Lab', 'lab'),
+        ('Microprocessors Lab', 'lab')
+    ]
+
+    S5 = [
+        ('Software Engineering', 'theory'),
+        ('System Software', 'theory'),
+        ('Operating System', 'theory'),
+        ('Database Management Systems', 'theory'),
+        ('Computer Networks 1', 'theory'),
+        ('Formal Languages and Automata Theory', 'theory'),
+        ('Database Lab', 'lab'),
+        ('Operating Systems Lab', 'lab')
+    ]
+
+    S6 = [
+        ('Management and Entrepreneurship', 'theory'),
+        ('UNIX System Programming', 'theory'),
+        ('Compiler Design', 'theory'),
+        ('Computer Networks 2', 'theory'),
+        ('Computer Graphics and Visualization', 'theory'),
+        ('Operations Research', 'theory'),
+        ('Computer Graphics and Visualization Lab', 'lab'),
+        ('Compiler Design Lab', 'lab')
+    ]
+
+    S7 = [
+        ('Objected Oriented Design', 'theory'),
+        ('Embedded Computing', 'theory'),
+        ('Programming the Web', 'theory'),
+        ('Data Warehousing and Data Mining', 'theory'),
+        ('Advanced DBMS', 'theory'),
+        ('JAVA and J2EE', 'theory'),
+        ('JAVA Lab', 'lab'),
+        ('Web Lab', 'lab')
+    ]
+
+    S8 = [
+        ('Software Architectures', 'theory'),
+        ('System Modelling and Simulation', 'theory'),
+        ('Wireless Networks', 'theory'),
+        ('User Interface Design', 'theory'),
+        ('Information Theory and Coding', 'theory'),
+        ('Network Management Systems', 'theory'),
+        ('Project Work', 'lab'),
+        ('Seminar', 'lab')
+    ]
+
+    CS = [S1, S2, S3, S4, S5, S6, S7, S8]
+
+
+class CIVIL(object):
+    S1 = [
+        ('Engineering Mathematics 1', 'theory'),
+        ('Engineering Physics', 'theory'),
+        ('Engineering Chemistry', 'theory'),
+        ('Elements of Civil engineering', 'theory'),
+        ('Programming in C and Data structures', 'theory'),
+        ('Elements of Mechanical Engineering', 'theory'),
+        ('Chemistry Lap', 'lab'),
+        ('Computer Lab', 'lab')
+    ]
+
+    S2 = [
+        ('Basic Electronics', 'theory'),
+        ('Computer Aided Drawing', 'theory'),
+        ('Basic Electrical Engineering', 'theory'),
+        ('Engineering Mathematics 2', 'theory'),
+        ('English', 'theory'),
+        ('Constitution', 'theory'),
+        ('Physics Lab', 'lab'),
+        ('Workshop', 'lab')
+    ]
+
+    S3 = [
+        ('Engineering Mathematics 3', 'theory'),
+        ('Building Materials and Construction', 'theory'),
+        ('Strength of Materials', 'theory'),
+        ('Surveying 1', 'theory'),
+        ('Fluid Mechanics', 'theory'),
+        ('Applied Engineering Geology', 'theory'),
+        ('Basic Material Testing Lab', 'lab'),
+        ('Surveying Lab 1', 'lab')
+    ]
+
+    S4 = [
+        ('Engineering Mathematics 4', 'theory'),
+        ('Concrete Technology', 'theory'),
+        ('Structural Analysis', 'theory'),
+        ('Surveying 2', 'theory'),
+        ('Hydraulics and Hydraulic Machines', 'theory'),
+        ('Building and Planning and Drawing', 'theory'),
+        ('Surveying Lab 2', 'lab'),
+        ('Applied Engineering and Geology Lab',  'lab')
+    ]
+
+    S5 = [
+        ('Management and Entrepreneurship', 'theory'),
+        ('Design of RCC Structural Elements', 'theory'),
+        ('Structural Analysis 2', 'theory'),
+        ('Geo technical Engineering 1', 'theory'),
+        ('Hydrology and Irrigation Engineering', 'theory'),
+        ('Transportation Engineering 1', 'theory'),
+        ('Hydraulics Lab', 'lab'),
+        ('Computer Aided Design Lab', 'lab')
+    ]
+
+    S6 = [
+        ('Environmental Engineering', 'theory'),
+        ('Design and Drawing of RC Structures', 'theory'),
+        ('Transportation Engineering 2', 'theory'),
+        ('Geo technical Engineering', 'theory'),
+        ('Hydraulic Structures and Irrigation Drawing', 'theory'),
+        ('Theory of Elasticity', 'theory'),
+        ('Geo Technical Lab', 'lab'),
+        ('Extensive Survey Lab', 'lab')
+    ]
+
+    S7 = [
+        ('Environmental Engineering 2', 'theory'),
+        ('Design of Steel Structures', 'theory'),
+        ('Estimation and Valuation', 'theory'),
+        ('Design of Pre-Stressed Concrete Structures', 'theory'),
+        ('Advanced design of RC Structures', 'theory'),
+        ('Design of Masonry Structures', 'theory'),
+        ('Environmental Engineering Lab', 'lab'),
+        ('Concrete and Highway Materials Lab', 'lab')
+    ]
+
+    S8 = [
+        ('Advanced Concrete Technology', 'theory'),
+        ('Design and Drawing of Steel Structures', 'theory'),
+        ('Advanced Pre Stressed Concrete Structures', 'theory'),
+        ('Pavement Design', 'theory'),
+        ('Advanced Foundation Design', 'theory'),
+        ('Industrial Waste Water Treatment', 'theory'),
+        ('Project Work', 'lab'),
+        ('Seminar', 'lab')
+    ]
+
+    CIVIL = [S1, S2, S3, S4, S5, S6, S7, S8]
